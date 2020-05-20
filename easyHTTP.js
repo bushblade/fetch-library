c414380c95870b98c4fc7b6f3daf6289a2e4c864
Will Adams <bushblade@gmail.com>
2020-05-20 17:15:00 +0100

    export

--- easyHTTP.js
+++ easyHTTP.js
@@ -2,28 +2,30 @@ const easyHTTP = (baseUrl = '', headers = {}) => {
   const sharedFetch = async (method, endPoint, data = null) => {
     const config = {
       method,
-      headers: {'content-type': 'application/json', ...headers},
-    };
-    if (data) config.body = JSON.stringify(data);
+      headers: { 'content-type': 'application/json', ...headers }
+    }
+    if (data) config.body = JSON.stringify(data)
     const response = await fetch(`${baseUrl}${endPoint}`, config).then(res => {
-      if (!res.ok) throw Error(`${res.status} message: ${res.statusText}`);
-      return res.json();
-    });
-    return response;
-  };
+      if (!res.ok) throw Error(`${res.status} message: ${res.statusText}`)
+      return res.json()
+    })
+    return response
+  }
 
   return {
     get(endPoint) {
-      return sharedFetch('GET', endPoint);
+      return sharedFetch('GET', endPoint)
     },
     post(endPoint, data) {
-      return sharedFetch('POST', endPoint, data);
+      return sharedFetch('POST', endPoint, data)
     },
     put(endPoint, data) {
-      return sharedFetch('PUT', endPoint, data);
+      return sharedFetch('PUT', endPoint, data)
     },
     delete(endPoint) {
-      return sharedFetch('DELETE', endPoint);
-    },
-  };
-};
+      return sharedFetch('DELETE', endPoint)
+    }
+  }
+}
+
+export default easyHTTP

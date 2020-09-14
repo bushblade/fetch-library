0090e1ab5c54ec0d868f2abed7c65e29395cd80d
Will Adams <bushblade@gmail.com>
2020-09-14 15:40:16 +0100

    typo

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const easyHTTP = (baseUrl = '', headers = {}) => {
   const sharedFetch = async (method, endPoint, data = null) => {
     const config = {
       method,
-      headers: { 'content-type': 'application/json', ...headers }
+      headers: { 'Content-Type': 'application/json', ...headers }
     }
     if (data) config.body = JSON.stringify(data)
-    const response = await fetch(`${baseUrl}${endPoint}`, config).then(res => {
-      if (!res.ok) throw Error(`${res.status} message: ${res.statusText}`)
-      return res.json()
-    })
+    const response = await fetch(`${baseUrl}${endPoint}`, config).then(
+      (res) => {
+        if (!res.ok) throw Error(`${res.status} message: ${res.statusText}`)
+        return res.json()
+      }
+    )
     return response
   }
 
